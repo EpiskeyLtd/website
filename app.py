54a7d50ba52835c9b476b3a97027e57eab540e7e
Nevelina <nevelina.aleksandrova@gmail.com>
2015-03-13 14:10:09 +0000

    move favicon

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.png', mimetype='image/png')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
