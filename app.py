4eeb9e0a179d2144fd5fa4dbb0219defce9a9e3e
Nevelina <nevelina.aleksandrova@gmail.com>
2015-03-13 13:33:21 +0000

    Changes to ports for heroku

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
