d9501084488656b4d58b68c7dc85240d107cf451
Nevelina <nevelina.aleksandrova@gmail.com>
2015-03-13 14:13:32 +0000

    changed icon type

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.png', mimetype='image/png')
-
+    
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
